129029c4322fcde0c04720e083842607dc1959c1
SMRITI <114291555+smriti2911@users.noreply.github.com>
2024-06-11 23:19:57 +0530

    Add files via upload

---
+++ rock_paper_scissor.py
@@ -0,0 +1,50 @@
+import random
+Rock= '''
+          _______
+      ---'   ____)
+            (_____)
+            (_____)
+    VK      (____)
+      ---.__(___)
+
+'''
+Paper='''
+          _______
+      ---'   ____)____
+                ______)
+                _______)
+    VK         _______)
+      ---.__________)
+'''
+Scissors='''
+          _______
+      ---'   ____)____
+                ______)
+             __________)
+    VK      (____)
+      ---.__(___)
+'''
+game_image =[Rock, Paper, Scissors]
+
+while True:
+    user_choice= int(input("what do you choose? type 0 for rock, 1 for paper, 2 for scissors\n"))
+    if user_choice >=3 or user_choice<0:
+        print("you typed an invalid number, You lose!")
+    else:
+        print(game_image[user_choice])
+        computer_choice= random.randint(0,2)
+        print("computer chose:")
+        print(game_image[computer_choice])
+        if user_choice==0 and computer_choice==2:
+            print("you win!")
+        elif computer_choice==0 and user_choice==2:
+            print("you lose!")
+        elif computer_choice> user_choice:
+            print("you lose!")
+        elif user_choice> computer_choice:
+            print("you win!")
+        elif computer_choice== user_choice:
+            print(" it's a draw")
+        
+           
+        
